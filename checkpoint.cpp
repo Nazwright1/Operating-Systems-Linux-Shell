b6d66c3cd0d6c7a7b3b5317a8469b33e86121d4d
Nazwright1 <43227068+Nazwright1@users.noreply.github.com>
2020-11-12 10:45:47 -0500

    changes

---
+++ checkpoint.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cstring>
+#include <vector>
+#include <iostream>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+
+
+void printPrompt(){
+  std::cout<< ">"
+}
