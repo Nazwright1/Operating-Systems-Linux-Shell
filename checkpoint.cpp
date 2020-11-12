8f4f93f934467065f60b3ebf0567ded18bac8740
Nazwright1 <43227068+Nazwright1@users.noreply.github.com>
2020-11-12 11:28:03 -0500

    created shell prompt and interactive input

--- checkpoint.cpp
+++ checkpoint.cpp
@@ -6,8 +6,44 @@
 #include <signal.h>
 #include <sys/wait.h>
 
+// ash directory
+const char* ash = "ash";
+int numArguments = 0;
+
+void printPrompt(char* directory){
+  if(directory){
+    std::cout<< directory << " >";
+  }
+  else{
+  	std::cout<< "print error";
+  	exit(1);
+  }
+}
+
+char* getCommands(){
+  char* commandLine = (char*)malloc( FILENAME_MAX );
+  std::cin.getline(commandLine, FILENAME_MAX);
+  return commandLine;
+}
+
+//char** parseCommands(char* commands){
+  
+//}
 
 
-void printPrompt(){
-  std::cout<< ">"
+
+
+int main(){
+  char* current_directory = strdup(ash);
+  while(1){
+     printPrompt(current_directory);
+     char* commands = getCommands();
+     if(strlen(commands) != 0){
+       std::cout <<  commands << "\n";
+     }
+     else{
+       continue;
+     }
+   
+  }
 }
