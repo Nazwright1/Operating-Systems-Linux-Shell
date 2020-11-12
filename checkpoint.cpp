e4453d83ce11949b34ddbcf306c7fe7959ee3853
Nazwright1 <43227068+Nazwright1@users.noreply.github.com>
2020-11-12 12:15:32 -0500

    create set home directory function

--- checkpoint.cpp
+++ checkpoint.cpp
@@ -5,11 +5,15 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <libgen.h>
 
 // ash directory
 const char* ash = "ash";
 int numArguments = 0;
+char home[FILENAME_MAX];
 
+
+// print the current directory
 void printPrompt(char* directory){
   if(directory){
     std::cout<< directory << " >";
@@ -20,6 +24,30 @@ void printPrompt(char* directory){
   }
 }
 
+// takes a directory name and adds slashes
+char* formatDirectory(char* directory){
+  const char* slash = "/";
+  char* temp = strdup(slash);
+  strcat(temp, directory);
+  strcat(temp, slash);
+  return temp; 
+}
+
+// gets the dir name of the current directory
+char* getDirectoryname(char* dir_path){
+	// if path is /user/username/ then this returns username
+  return basename(dir_path);
+}
+
+void setHomeDirectory(){
+   getcwd(home, sizeof(home));
+}
+
+
+
+
+
+// get a char* of commands from the prompt
 char* getCommands(){
   char* commandLine = (char*)malloc( FILENAME_MAX );
   std::cin.getline(commandLine, FILENAME_MAX);
@@ -28,6 +56,12 @@ char* getCommands(){
 
 //char** parseCommands(char* commands){
   
+ // char *prog = strtok(commands, " ");
+ // char *tmp = prog;
+ // while(tmp != NULL) {
+    
+   // tmp = strtok(NULL, " ");
+  //}
 //}
 
 
@@ -36,12 +70,17 @@ char* getCommands(){
 int main(){
   char* current_directory = strdup(ash);
   while(1){
+  	 setHomeDirectory();
      printPrompt(current_directory);
+     // need to free this once done.
      char* commands = getCommands();
      if(strlen(commands) != 0){
-       std::cout <<  commands << "\n";
+     char* newhome = getDirectoryname(home);
+     std::cout << formatDirectory(newhome);
+       
      }
      else{
+       // print the prompt
        continue;
      }
    
